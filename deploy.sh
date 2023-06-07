4b626823202c0d44768954e75372fb163434b9c3
Sully Sullenberger <sully@sadburger.com>
2023-06-07 12:44:20 -0700

    add docker ps

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 source prod.env.sh
 echo $DOMAIN
 git pull && docker build . -t emilytarot:latest && docker stack deploy -c docker-compose.prod.yaml emilytarot && docker service update emilytarot_emilytarot --force
+
+docker ps
