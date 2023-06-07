65363b3d51ef8da4e2967d4ba5814ec8161a6319
Sully Sullenberger <sully@sadburger.com>
2023-06-07 12:43:44 -0700

    force service update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 source prod.env.sh
 echo $DOMAIN
-git pull && docker build . -t emilytarot:latest && docker stack deploy -c docker-compose.prod.yaml emilytarot
+git pull && docker build . -t emilytarot:latest && docker stack deploy -c docker-compose.prod.yaml emilytarot && docker service update emilytarot_emilytarot --force
